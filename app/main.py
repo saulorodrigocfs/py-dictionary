8f05990d73c4a7196a5dd39c1b17420e403ad1b9
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2026-02-12 20:41:33 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
 class Dictionary:
 
-    def __init__(self, capacity=8):
+    def __init__(self, capacity: int = 8) -> None:
         self.capacity = capacity
         self.size = 0
         self.table = [None] * self.capacity
-    
-    def _get_index(self, key):
+
+    def _get_index(self, key: int) -> int:
         return hash(key) % self.capacity
-    
-    def __setitem__(self, key, value):
+
+    def __setitem__(self, key: int, value: int) -> int:
         index = self._get_index(key)
         node = self.table[index]
-        
+
         if node is None:
             self.table[index] = [key, hash(key), value]
             self.size += 1
@@ -19,7 +19,7 @@ class Dictionary:
         elif node[0] == key:
             self.table[index][2] = value
             return
-        
+
         i = (index + 1) % self.capacity
         while self.table[i] is not None:
             if self.table[i][0] == key:
@@ -29,7 +29,7 @@ class Dictionary:
         self.table[i] = [key, hash(key), value]
         self.size += 1
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int) -> int:
         index = self._get_index(key)
         node = self.table[index]
         if node is None:
@@ -43,5 +43,5 @@ class Dictionary:
             i = (i + 1) % self.capacity
         raise KeyError(key)
 
-    def __len__(self):
+    def __len__(self) -> None:
         return self.size
